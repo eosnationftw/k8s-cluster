07eca275ac6a2a30ed904a3b14f68fdb6961f146
Guillaume Cléroux <guillaume.cleroux@protonmail.com>
2024-01-03 17:44:01 -0500

    Add pvc config for clickhouse

--- flake.nix
+++ flake.nix
@@ -1,20 +1,14 @@
 {
   description = "K8S Firehose";
-  nixConfig.bash-prompt = "[nix(k8s-firehose)] >> ";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "K8S Firehose";
-        buildInputs = [ pkgs.fluxcd pkgs.cilium-cli pkgs.kubectl ];
-        shellHook = ''
-          echo "========================================================="
-          echo "Nix shell for $name"
-          echo "Make sure you read the README before testing the cluster."
-          echo "========================================================="
-        '';
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ cilium-cli fluxcd kubectl k9s ];
+        };
+      });
 }
